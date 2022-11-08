a11ecd55209317beadb17f532ccbcc50f91acac9
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2022-11-08 15:37:16 +0900

    Create Project
    Step1. Create project

---
+++ app/src/main/java/com/prosoft/todaydiary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prosoft.todaydiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
